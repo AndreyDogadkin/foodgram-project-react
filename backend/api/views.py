9c455509b24efa2f8d17825a3a71eff791ce13d8
Andey Dogadkin <jvgger@gmail.com>
2023-11-24 02:24:11 +0300

    add templates

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.permissions import SAFE_METHODS
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from api.serializers import RecipeReadSerializer, RecipeSerializer
+from recipes.models import Recipe
+
+
+class RecipeViewSet(ModelViewSet):
+
+    queryset = Recipe.objects.all()
+    # permission_classes = ...
+    # filter_backends = ...
+    # pagination_class = ...
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return RecipeReadSerializer
+        return RecipeSerializer
