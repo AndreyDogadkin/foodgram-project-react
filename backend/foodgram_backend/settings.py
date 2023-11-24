e942de70fc757a87956194f1ca1ef83d642b45b6
Andey Dogadkin <jvgger@gmail.com>
2023-11-24 20:56:07 +0300

    added recipe viewset

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters'
+    'django_filters',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
     'recipes.apps.RecipesConfig',
